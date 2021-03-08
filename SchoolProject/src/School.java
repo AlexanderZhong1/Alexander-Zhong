ce94f58c5c3f1ae76fb281452655534d1012739b
Alexander Zhong <1328294@learn.vsb.bc.ca>
2021-03-08 10:20:22 -0800

    ++

--- SchoolProject/src/School.java
+++ SchoolProject/src/School.java
@@ -3,5 +3,65 @@ import java.util.ArrayList;
 public class School {
     ArrayList<Teacher> teachers = new ArrayList<>();
     ArrayList<Student> students = new ArrayList<>();
+    public String schoolName;
+    public int yearEstablished;
+    public int numOfStudents;
+
+    public School(String schoolName, int yearEstablished, int numOfStudents) {
+        this.schoolName = schoolName;
+        this.yearEstablished = yearEstablished;
+        this.numOfStudents = numOfStudents;
+    }
+
+    public void setNumOfStudents(int numOfStudents) {
+        this.numOfStudents = numOfStudents;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void setYearEstablished(int yearEstablished) {
+        this.yearEstablished = yearEstablished;
+    }
+
+    public int getNumOfStudents() {
+        return numOfStudents;
+    }
+
+    public int getYearEstablished() {
+        return yearEstablished;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void addTeacher(String firstName, String lastName, String subject) {
+        teachers.add(new Teacher(firstName, lastName, subject));
+    }
+
+    public void deleteTeacher(String firstName) {
+        for (int x = 0; x < teachers.size(); x++) {
+            if (teachers.get(x).getFirstName() == firstName) {
+                teachers.remove(x);
+                break;
+            }
+        }
+    }
+
+    public void addStudent(String firstName, String lastName, int grade) {
+        students.add(new Student(firstName, lastName, grade));
+    }
+
+    public void deleteStudent(String firstName) {
+        for (int x = 0; x < students.size(); x++) {
+            if (students.get(x).getFirstName() == firstName) {
+                students.remove(x);
+                break;
+            }
+        }
+    }
+
 
 }
--- SchoolProject/src/Student.java
+++ SchoolProject/src/Student.java
@@ -1,3 +1,53 @@
-public class Student {
+import java.util.UUID;
 
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int grade;
+    public String studentID = UUID.randomUUID().toString();
+
+    public Student(String firstName, String lastName, int grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    // sets the last name of the student
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // sets the first name of the student
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // sets the grade of the student
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    // gets the last name of the student
+    public String getLastName() {
+        return lastName;
+    }
+
+    // gets the first name of the student
+    public String getFirstName() {
+        return firstName;
+    }
+    // returns the grade
+    public int getGrade() {
+        return grade;
+    }
+
+    // returns the student id
+    public String getStudentID() {
+        return studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + firstName + " " + lastName + " Grade: " + grade;
+    }
 }
