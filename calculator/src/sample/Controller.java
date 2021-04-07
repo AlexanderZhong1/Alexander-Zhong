375a3bd438b4bbd15289a44d62f238d15352e859
Alexander Zhong <1328294@learn.vsb.bc.ca>
2021-04-07 09:34:42 -0700

    ++

--- calculator/src/sample/Controller.java
+++ calculator/src/sample/Controller.java
@@ -1,4 +1,66 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+
+    public TextArea textDisplay;
+
+
+    public void buttonDecimal(ActionEvent actionEvent) {
+    }
+
+    public void button0(ActionEvent actionEvent) {
+    }
+
+    public void buttonEqual(ActionEvent actionEvent) {
+    }
+
+    public void buttonPlus(ActionEvent actionEvent) {
+    }
+
+    public void button1(ActionEvent actionEvent) {
+    }
+
+    public void button2(ActionEvent actionEvent) {
+    }
+
+    public void button3(ActionEvent actionEvent) {
+    }
+
+    public void buttonMinus(ActionEvent actionEvent) {
+    }
+
+    public void button4(ActionEvent actionEvent) {
+    }
+
+    public void button5(ActionEvent actionEvent) {
+    }
+
+    public void button6(ActionEvent actionEvent) {
+    }
+
+    public void buttonMultiply(ActionEvent actionEvent) {
+    }
+
+    public void button7(ActionEvent actionEvent) {
+    }
+
+    public void button8(ActionEvent actionEvent) {
+    }
+
+    public void button9(ActionEvent actionEvent) {
+    }
+
+    public void buttonDivide(ActionEvent actionEvent) {
+    }
+
+    public void buttonC(ActionEvent actionEvent) {
+    }
+
+    public void buttonDelete(ActionEvent actionEvent) {
+    }
 }
