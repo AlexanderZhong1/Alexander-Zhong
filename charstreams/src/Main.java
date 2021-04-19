54d175b352dfa5cd6021008c997a82508e3352af
Alexander Zhong <1328294@learn.vsb.bc.ca>
2021-04-19 10:22:22 -0700

    ++

--- charstreams/src/Main.java
+++ charstreams/src/Main.java
@@ -1,7 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
+        ArrayList<String> lines = new ArrayList<>();
+        FileReader fr = new FileReader("ProgrammingHistory.txt");
+        BufferedReader = new BufferedReader(fr);
+
+
 
 
 
