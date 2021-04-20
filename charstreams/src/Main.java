f35307ea56e7e8cc8d3b687f2bdac3befef72ea3
Alexander Zhong <1328294@learn.vsb.bc.ca>
2021-04-20 09:09:34 -0700

    finished file reading assignment

--- charstreams/src/Main.java
+++ charstreams/src/Main.java
@@ -4,11 +4,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
+
+
+
 
         ArrayList<String> lines = new ArrayList<>();
         FileReader fr = new FileReader("ProgrammingHistory.txt");
-        BufferedReader = new BufferedReader(fr);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+            lines.add(line);
+        }
+
+
+
+
+
+        br.close();
+
+
+        System.out.println(wordSearch("is", lines));
+
 
 
 
@@ -16,4 +38,18 @@ public class Main {
 
 
     }
+
+    static ArrayList<Integer> wordSearch(String word, ArrayList<String> lizt) {
+        ArrayList<Integer> solution = new ArrayList<>();
+        for (int i = 0; i < lizt.size(); i++) {
+            if (lizt.get(i).contains(word)) {
+                solution.add(i);
+            }
+
+        }
+
+        return solution;
+    }
+
+
 }
